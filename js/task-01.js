9d1c36e9f2dfc0c129ae329f654b287ceb810afc
Luydmyla <lytic.ok@gmail.com>
2021-12-04 01:19:28 +0200

    add task 2
    -update task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,23 @@
-const listElem = document.querySelectorAll('.item');
-console.log(`Number of categories: ${listElem.length}`);
+// const listElem = document.querySelectorAll('.item');
 // console.log(listElem);
-for (let elem of listElem) {
-    const listTitle = elem.querySelector('h2');
-    console.log('Category:', listTitle.textContent);
-    const listChild = elem.querySelectorAll('li');
+// console.log(`Number of categories: ${listElem.length}`);
+// console.log(listElem);
+// for (let elem of listElem) {
+//     const listTitle = elem.querySelector('h2');
+//     console.log('Category:', listTitle.textContent);
+//     const listChild = elem.querySelectorAll('li');
+//     console.log('Elements:', listChild.length);
+// }
+
+const listEl = document.querySelectorAll('.item');
+// console.log(listEl);
+console.log(`Number of categories: ${listEl.length}`);
+listEl.forEach(element => {
+    // console.log(element);
+    const TitleEl = element.firstElementChild;
+    console.log('Category:', TitleEl.textContent);
+     const listChild = element.querySelectorAll('li');
+     // const liElChild = element.lastElementChild;
+    // console.log(liElChild);
     console.log('Elements:', listChild.length);
-}
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+console.log(ingredients);
+const ingredientsElem = document.querySelector('ul');
+ingredientsElem.classList.add('ingredients');
+console.log(ingredientsElem);
+const liElements = ingredients.map(ingredient => {
+const listElem = document.createElement('li');
+listElem.textContent = (`${ingredient}`) ;
+listElem.classList.add('item');
+  return listElem;
+})
+console.log(liElements);
+ingredientsElem.append(...liElements);
+ 
