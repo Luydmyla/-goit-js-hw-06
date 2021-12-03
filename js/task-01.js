1e5c43f6d5502fd65986b6f4392a89f1288c9a98
Luydmyla <lytic.ok@gmail.com>
2021-12-03 02:24:06 +0200

    Add task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,9 @@
-const menuItemsByClass = document.querySelectorAll(".menu-item");
-console.log(menuItemsByClass);
+const listElem = document.querySelectorAll('.item');
+console.log(`Number of categories: ${listElem.length}`);
+// console.log(listElem);
+for (let elem of listElem) {
+    const listTitle = elem.querySelector('h2');
+    console.log('Category:', listTitle.textContent);
+    const listChild = elem.querySelectorAll('li');
+    console.log('Elements:', listChild.length);
+}
