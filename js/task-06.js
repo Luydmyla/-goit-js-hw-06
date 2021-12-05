6f8acc9433d64e99e7707b542d573d4212335346
Luydmyla <lytic.ok@gmail.com>
2021-12-05 22:41:58 +0200

    add task -6

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+const inputElem = document.querySelector('#validation-input');
+// console.log(inputElem);
+// console.log(inputElem.dataset.length);
+inputElem.addEventListener('blur', onInputBlur );
+function onInputBlur(event) {
+    // console.log(event.currentTarget.value.length);
+    inputElem.classList.add('#validation-input');
+    if (event.currentTarget.value.length == inputElem.dataset.length) {
+        inputElem.classList.remove('invalid');
+        inputElem.classList.add('valid');
+        // console.log(inputElem);
+    } else {
+        inputElem.classList.remove('valid');
+        inputElem.classList.add('invalid');
+        // console.log(inputElem);
+        event.currentTarget.value = '';
+    }
+}
