f929f8a9d4d6bcfde9a22af9b1c1e35678b74c38
Luydmyla <lytic.ok@gmail.com>
2021-12-06 04:04:15 +0200

    started task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,48 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonCreateElem = document.querySelector('button[data-create]');
+console.log(buttonCreateElem);
+const buttonDestroyElem = document.querySelector('button[data-destroy]');
+console.log(buttonDestroyElem);
+const inputElem = document.querySelector('input');
+console.log(inputElem);
+const boxesElem = document.querySelector('#boxes');
+console.log(boxesElem);
+
+function onInput(event) {
+  const amount = event.currentTarget.value;
+  console.log(amount);
+  return amount;
+ };
+function createBoxes(amount) {
+  
+}
+function onClickCreate() {
+  // let i = 0;
+  // while (i < amount) {
+  //   createBoxes(i);
+  const box = document.createElement("div");
+  box.classList.add("new-box");
+  box.style.width = "30px";
+  box.style.height = "30px";
+  box.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    boxesElem.append(box);
+    // i += 1;
+    console.log(box);
+    
+    // }
+  
+};
+
+function onClickDestroy() {
+  const newBox = document.querySelector(".new-box");
+  console.log(newBox);
+  newBox.remove();
+//  console.log(event.currentTarget.value); 
+
+};
+
+inputElem.addEventListener('input', onInput );
+buttonCreateElem.addEventListener('click', onClickCreate);
+buttonDestroyElem.addEventListener('click', onClickDestroy );
