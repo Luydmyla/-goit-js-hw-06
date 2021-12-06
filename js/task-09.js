c8e725506beb061a2d1c33bf7d6a16dfbf22a27e
Luydmyla <lytic.ok@gmail.com>
2021-12-06 02:15:22 +0200

    add task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const bodyElem = document.querySelector('body');
+// console.log(bodyElem);
+const buttonElem = document.querySelector('button');
+// console.log(buttonElem);
+const spanElem = document.querySelector('.color');
+// console.log(spanElem);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+buttonElem.addEventListener('click', onClickButton);
+function onClickButton() {
+   bodyElem.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//  console.log(bodyElem.style.backgroundColor); 
+   spanElem.textContent = bodyElem.style.backgroundColor;
+   console.log(spanElem.textContent) ;
+};
