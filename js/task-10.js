03a271c9677010576f226d410eceddd5ae3e5522
Luydmyla <lytic.ok@gmail.com>
2021-12-06 19:06:13 +0200

    add task - 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,47 +2,42 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const buttonCreateElem = document.querySelector('button[data-create]');
-console.log(buttonCreateElem);
+// console.log(buttonCreateElem);
 const buttonDestroyElem = document.querySelector('button[data-destroy]');
-console.log(buttonDestroyElem);
+// console.log(buttonDestroyElem);
 const inputElem = document.querySelector('input');
-console.log(inputElem);
+// console.log(inputElem);
 const boxesElem = document.querySelector('#boxes');
-console.log(boxesElem);
+// console.log(boxesElem);
 
 function onInput(event) {
-  const amount = event.currentTarget.value;
-  console.log(amount);
-  return amount;
- };
-function createBoxes(amount) {
-  
-}
-function onClickCreate() {
-  // let i = 0;
-  // while (i < amount) {
-  //   createBoxes(i);
-  const box = document.createElement("div");
-  box.classList.add("new-box");
-  box.style.width = "30px";
-  box.style.height = "30px";
-  box.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    boxesElem.append(box);
-    // i += 1;
-    console.log(box);
-    
-    // }
-  
+  const inputValue = event.currentTarget.value;
+  console.log(inputValue);
+  return inputValue;
 };
 
-function onClickDestroy() {
-  const newBox = document.querySelector(".new-box");
-  console.log(newBox);
-  newBox.remove();
-//  console.log(event.currentTarget.value); 
+function createBoxes(amount) {
+  console.log(inputElem.value);
+  let width = 30;
+let height = 30;
+  for (let i = 1; i <= inputElem.value; i += 1) {
+  const box = document.createElement("div");
+  box.classList.add("new-box");
+  box.style.width = `${width}px`;
+  box.style.height =`${height}px`;
+  box.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  boxesElem.append(box); 
+   width += 10;
+   height += 10;
+   console.log(box);
+ }
+ }
 
+function onClickDestroy() {
+inputElem.value = "";
+boxesElem.innerHTML = "";
 };
 
 inputElem.addEventListener('input', onInput );
-buttonCreateElem.addEventListener('click', onClickCreate);
+buttonCreateElem.addEventListener('click', createBoxes);
 buttonDestroyElem.addEventListener('click', onClickDestroy );
